e9a309757f1c42feae6fde26a6232cf7f4bb2780
John Elder <johne4196@gmail.com>
2022-09-01 11:00:43 -0700

    Initial Commit

---
+++ firstly/android/app/src/main/kotlin/com/example/firstly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firstly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ secondly/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Codemy.com"),
+          ),
+        ),
+        body: Center(
+          child: Text("Learn To Code At Codemy.com!!"),
+        ),
+      ),
+    ),
+  );
+}
