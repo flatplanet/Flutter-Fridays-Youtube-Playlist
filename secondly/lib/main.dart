34effa5a474605d14962433cfe9ed703f5b6ca74
John Elder <johne4196@gmail.com>
2022-09-29 09:11:02 -0700

    hot reloading stateless widgets

--- secondly/lib/main.dart
+++ secondly/lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Center(
             child: Text("Codemy.com"),
           ),
-          backgroundColor: Colors.deepPurple[400],
+          backgroundColor: Color.fromARGB(255, 17, 194, 47),
         ),
         body: const Center(
           child: Image(image: AssetImage('images/aspen.png')),
         ),
-        backgroundColor: Colors.deepPurple[200],
+        backgroundColor: Color.fromARGB(255, 167, 224, 141),
       ),
-    ),
-  );
+    );
+  }
 }
