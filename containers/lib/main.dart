9c3608582f9d3bb2161f44741ae7a1210d2b6550
John Elder <johne4196@gmail.com>
2022-10-14 09:24:53 -0700

    SafeArea

--- containers/lib/main.dart
+++ containers/lib/main.dart
@@ -11,20 +11,22 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("Codemy.com"),
+        //appBar: AppBar(
+        //  title: const Center(
+        //    child: Text("Codemy.com"),
+        //  ),
+        //  backgroundColor: Color.fromARGB(255, 17, 194, 47),
+        //),
+        body: SafeArea(
+          child: Container(
+            color: Colors.teal,
+            width: 150.0,
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(20.0),
+            //margin: EdgeInsets.all(150.0),
+            child: Text("Yo! What's going on?!"),
           ),
-          backgroundColor: Color.fromARGB(255, 17, 194, 47),
-        ),
-        body: Container(
-          color: Colors.teal,
-          width: 150.0,
-          height: 150.0,
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(20.0),
-          margin: EdgeInsets.all(150.0),
-          child: Text("Yo! What's going on?!"),
         ),
       ),
     );
